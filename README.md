56a06c6ed6bc9c1cc4770f81cec470801395b0c6
yashsainani <yashsainani1803@gmail.com>
2024-10-29 13:05:35 +0530

    created some changes for betterment

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Coming Soon #
+# Add to Cart Project
+
 [Add To Cart Preview](https://yashsainani.github.io/add-to-cart/)
+
+## Overview
+
+This project implements an "Add to Cart" functionality for an e-commerce application. Users can browse products, add them to their shopping cart, and proceed to checkout. This feature is designed to provide a seamless shopping experience.
+
+## Features
+
+- View a list of available products
+- Add products to the shopping cart
+- Remove products from the shopping cart
+- Display the total price of items in the cart
+- Responsive design for mobile and desktop
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/yashsainani/add-to-cart
--- script.js
+++ script.js
@@ -6,50 +6,98 @@ const cartContainer = document.querySelector('.dynamic-div-2');
 const total = document.getElementById('totalPrice');
 
 function addProduct() {
-    const product = productName.value;
-    const price = productPrice.value;
-    if (product === "" || price === "" || +price < 0) {
+    const product = document.createElement('span');
+    product.innerText = productName.value;
+    const price = document.createElement('span');
+    price.innerText = productPrice.value;
+    if (product.innerText === "" || price.innerText === "" || +price.innerText < 0) {
         alert("Add some product with price");
+        document.getElementById('add-product').animate([
+            {transform : 'scale(1)'},
+            {transform : 'scale(0)'},
+            {transform : 'scale(1)'}
+        ], {
+            duration : 200,
+            easing : 'ease-in-out',
+            iterations : 2
+        });
         return;
     }
     const minusBtn = document.createElement('button');
     minusBtn.innerText = '-';
-    const quantity = '1';
+    const quantity = document.createElement('span');
+    quantity.innerText = 1;
     const addBtn = document.createElement('button');
     addBtn.innerText = '+';
     const btns = document.createElement('div');
-    btns.append(minusBtn, quantity, addBtn);
+    btns.append(minusBtn, quantity.innerText, addBtn);
+    btns.classList.add('btns');
 
     let productDiv = document.createElement('div');
-    productDiv.append(product);
-    productDiv.append(price);
-    productDiv.append(btns);
+    productDiv.appendChild(product);
+    productDiv.appendChild(price);
+    productDiv.appendChild(btns);
+    productDiv.className = 'product-div';
     productsContainer.appendChild(productDiv);
 
     addingInfo.style.display = 'none';
 
     let cartDiv = document.createElement('div');
     let amountDiv = document.createElement('div');
-    amountDiv.append(quantity, 'x', price);
-    cartDiv.append(product, amountDiv);
+    amountDiv.append(quantity.cloneNode(true), ' x ', price.cloneNode(true));
+    amountDiv.classList.add('amount-div');
+    cartDiv.append(product.cloneNode(true), amountDiv);
+    cartDiv.classList.add('cart-div');
 
     cartContainer.appendChild(cartDiv);
 
-    total.innerText = +total.innerText + +price;
+    total.innerText = +total.innerText + +price.innerText;
 
     minusBtn.addEventListener('click', () => {
-        updateChanges('-', quantity);
-    })
+        total.innerText = +total.innerText - +price.innerText;
+        quantity.innerText -= 1;
+        if (+quantity.innerText === 0) {
+            productDiv.remove();
+            cartDiv.remove();
+        }
+        else {
+            btns.innerHTML = "";
+            btns.append(minusBtn, quantity.innerText, addBtn);
+            cartDiv.innerHTML = "";
+            amountDiv.innerHTML = "";
+            amountDiv.append(quantity.cloneNode(true), ' x ', price.cloneNode(true));
+            amountDiv.classList.add('amount-div');
+            cartDiv.append(product.cloneNode(true), amountDiv);
+            cartDiv.classList.add('cart-div');
+        }
+
+        if (productsContainer.children.length === 0) {
+            addingInfo.style.display = 'block';
+            addingInfo.innerHTML = "You have vacated the cart let's add some new products to cart!!!"
+        }
+    });
+    
+    addBtn.addEventListener('click', () => {
+        total.innerText = +total.innerText + +price.innerText;
+        quantity.innerText = +quantity.innerText + 1;
+        btns.innerHTML = "";
+        btns.append(minusBtn, quantity.innerText, addBtn);
+        cartDiv.innerHTML = "";
+        amountDiv.innerHTML = "";
+        amountDiv.append(quantity.cloneNode(true), ' x ', price.cloneNode(true));
+        amountDiv.classList.add('amount-div');
+        cartDiv.append(product.cloneNode(true), amountDiv);
+        cartDiv.classList.add('cart-div');
+    });
 
     productName.value = "";
     productPrice.value = "";
 }
 
-function updateChanges(sign, ele) {
-    if (sign === '-') {
-        ele.innerText = +ele.innerText - +ele.innerText;
-        total.innerText = ele;
-    }
-}
+window.addEventListener('load', () => {
+    document.getElementsByTagName('header')[0].classList.add('onload');
+    document.querySelector('.products-container').classList.add('onload');
+    document.querySelector('.cart-container').classList.add('onload');
+});
 
 document.getElementById('add-product').addEventListener('click', addProduct);
